f973c44ae389064e925be069c566162d95704b25
hyehyehyehyehyehye <142138962+hyehyehyehyehyehye@users.noreply.github.com>
2023-11-01 10:34:52 +0900

    lottie변경

---
+++ js/lottie.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("lottie"), // Required
+  path: "../animation/ani4.json", // Required
+  renderer: "svg", // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+});
